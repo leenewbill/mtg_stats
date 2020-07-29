9ed2cfa8a42618c47d896174508232b81e8de5fd
Lee Newbill <15036418+leenewbill@users.noreply.github.com>
2020-07-29 11:38:21 -0700

    Apparently the Python community prefers a maximum line length of 79, so I made it happen.

--- mtg_stats.py
+++ mtg_stats.py
@@ -9,15 +9,19 @@ import csv
 import re
 
 # Program variables
-debug = False                        # set to True to print debug data
-driver_timeout = 100                 # seconds to wait for webpages containing JavaScript to load
-cardlist_filename = "cardlist.txt"   # input file containing list of cards to be processed
-csv_writer_filename = "mtg_out.csv"  # output file for writing CSV data
+debug = False                        # Set to True to print debug data
+driver_timeout = 100                 # Seconds to wait for webpages containing 
+                                     #   JavaScript to load
+cardlist_filename = "cardlist.txt"   # Input file containing list of cards to 
+                                     #   be processed
+csv_writer_filename = "mtg_out.csv"  # Output file for writing CSV data
 
-# Configure Chrome webdriver for headless operation (webpages not generated on the screen)
+# Configure Chrome webdriver for headless operation (webpages not generated on 
+#   the screen)
 options = Options()
 options.headless = True
-driver = webdriver.Chrome(options = options, executable_path = "./chromedriver")
+driver = webdriver.Chrome(options = options, executable_path = 
+        "./chromedriver")
 
 # Read cardlist (one MTG card entry per line) for processing
 with open(cardlist_filename, "r") as cardlist_file:
@@ -29,8 +33,8 @@ if debug:
 # Create an empty list to store our card data
 cards = []
 
-# Iterate through each card in the card list, collect useful data about each card, and store
-#       that data in a list
+# Iterate through each card in the card list, collect useful data about each 
+#   card, and store that data in a list
 for card in cardlist:
     print("Processing:", card)
 
@@ -50,12 +54,13 @@ for card in cardlist:
         # Use the Chrome webdriver with Selenium to open the webpage
         driver.get(url)
 
-        # Use Selenium to wait for the JavaScript code from EDHREC to generate the HTML we are 
-        #       looking for. If we time out, create an output entry for the card and throw an 
-        #       error. If the data is found, then continue processing the card.
+        # Use Selenium to wait for the JavaScript code from EDHREC to generate 
+        #   the HTML we are looking for. If we time out, create an output 
+        #   entry for the card and throw an error. If the data is found, 
+        #   then continue processing the card.
         try:
-            WebDriverWait(driver, driver_timeout).until(EC.presence_of_element_located((By.CLASS_NAME, 
-                    "card__label")))
+            WebDriverWait(driver, driver_timeout).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "card__label")))
         except:
             cards.append([card, "ERROR", "ERROR", "ERROR"])
             print("ERROR: WebDriverWait_timeout")
@@ -66,8 +71,9 @@ for card in cardlist:
             # Find the HTML tag that contains data we want
             find_div = soup.find('div', class_="card__label")
             
-            # Search for the number of EDHREC decks that use the card. The ".text" ignores the
-            #       HTML tag and just searches the content to be displayed on then screen.
+            # Search for the number of EDHREC decks that use the card. The 
+            #   ".text" ignores the HTML tag and just searches the content to 
+            #   be displayed on then screen.
             decks_search = re.search(r'In (\d+) decks', find_div.text)
 
             # If we found the data, copy it; otherwise, throw an error
@@ -80,22 +86,27 @@ for card in cardlist:
             # Find the HTML tag that contains the data we want
             find_div = soup.find('div', class_="card__price-border-crop")
 
-            # Search for the TCG price that's listed on EDHREC. (I'm unsure how this price is
-            #       calculated, so let's also go get the market price for the card from TCG.)
-            edhrec_tcg_price_search = re.search(r'\$.+(\$\d+\.\d\d)', find_div.text)
+            # Search for the TCG price that's listed on EDHREC. (I'm unsure how 
+            #   this price is calculated, so let's also go get the market price 
+            #   for the card from TCG.)
+            edhrec_tcg_price_search = re.search(r'\$.+(\$\d+\.\d\d)', 
+                find_div.text)
 
             # If we found the data, copy it; otherwise, throw an error
-            if edhrec_tcg_price_search:
+            if edhrec_tcg_price_search: 
                 edhrec_tcg_price = edhrec_tcg_price_search.group(1)
             else:
                 edhrec_tcg_price = "ERROR"
                 print("ERROR: edhrec_tcg_price_search")
 
-            # Search for the TCG URL on the card's EDHREC page. Since the data we want is inside
-            #       the HTML tag, we need to search the HTML as a string.
-            tcg_url_search = re.search(r'(https:\/\/store\.tcgplayer\.com.+?)\?', str(find_div))
+            # Search for the TCG URL on the card's EDHREC page. Since the data 
+            #   we want is inside the HTML tag, we need to search the HTML as a
+            #   string.
+            tcg_url_search = re.search(
+                r'(https:\/\/store\.tcgplayer\.com.+?)\?', str(find_div))
 
-            # If we did NOT find the data, throw an error; otherwise, copy it and continue processing
+            # If we did NOT find the data, throw an error; otherwise, copy it 
+            #   and continue processing
             if not tcg_url_search:
                 tcg_url = "ERROR"
                 tcg_market = "ERROR"
@@ -113,7 +124,8 @@ for card in cardlist:
                 find_div = soup.find('div', class_="price-point--market")
 
                 # Search for the card's market price
-                tcg_market_search = re.search(r'Normal\s+(\$\d+\.\d\d)', find_div.text)
+                tcg_market_search = re.search(r'Normal\s+(\$\d+\.\d\d)', 
+                    find_div.text)
 
                 # If we found the data, copy it; otherwise throw an error
                 if tcg_market_search:
@@ -128,10 +140,11 @@ for card in cardlist:
 if debug:
     print(cards)
         
-# Write the card data to a file in CSV format, which can be imported into a spreadsheet
+# Write the card data to a file in CSV format, which can be imported into a 
+#   spreadsheet
 with open(csv_writer_filename, "w", newline = '') as csvfile:
     csv_writer = csv.writer(csvfile, delimiter = ',', quotechar = "\"",
-            quoting = csv.QUOTE_MINIMAL)
+        quoting = csv.QUOTE_MINIMAL)
 
     # Write each card as a separate line
     for card in cards:
